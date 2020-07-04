2b6e94a791adef990569786aa28b277b9403ff83
dagelf <coenraad@wish.org.za>
2020-07-04 13:01:39 +0200

    doesn't belong in /usr/local/bin

--- install.sh
+++ install.sh
@@ -4,21 +4,28 @@ cd "$(dirname -- "$0")" 2>/dev/null # allows script to be called from another di
 
 #todo ask to install globally, then just sudo cp to /usr/local/bin and tee /etc/screenrc
 S=`which sudo`
-echo -n "I am ";whoami|grep root && S=""
-which wget >/dev/null || {
-  I="$S apt-get install -y"
-  $I wget
- }
-
 if $S whoami; then 
 echo Installing globally...
+echo -n "I am ";whoami|grep root && S=""
+which wget >/dev/null || {
+  echo Installing wget...
+  I="$S apt-get install -y"
+  $I wget 
+ }
+which earlyoom >/dev/null || {
+  echo Installing earlyoom...
+  I="$S apt-get install -y"
+  $I earlyoom
+ }
 # todo copy from defaults or make more modular
-grep -q ^kernel.sysrq=1 /etc/sysctl.conf || echo "kernel.sysrq=1" | $S tee -a /etc/sysctl.conf
+grep -q ^kernel.sysrq=1 /etc/sysctl.conf /etc/sysctl.d/*|| echo "kernel.sysrq=1" | $S tee -a /etc/sysctl.conf
+echo Trimming systemd journal...
 $S journalctl --vacuum-size=50M
-echo scripts
+echo Enable mouse scrolling in screenrc...
 $S cp scripts/* /usr/local/bin
 f=/etc/screenrc; echo $f
 grep -q '^termcapinfo xterm* ti@:te@' $f || echo 'termcapinfo xterm* ti@:te@'|$S tee -a $f
+echo Enable ssh socket reuse...
 f=/etc/ssh/ssh_config; echo $f
 if ! grep -q ControlPersist $f; then 
 cat <<EOF | $S tee -a $f
@@ -27,20 +34,21 @@ cat <<EOF | $S tee -a $f
 	ControlPersist 600
 EOF
 fi
-echo permissions and groups...
+echo Adding src group and updating /usr/src
 $S chgrp src /usr/src
 $S chmod g+s /usr/src
+echo Adding `whoami` to src...
 $S usermod -a -G src `whoami` # add current user to src group
 $S mkdir -p /usr/src/github # todo - make git pulls from github pull here under username
 else 
-echo Installing for `whoami` only...
+echo Installing for `whoami` only... you\'re missing out\!
 mkdir ~/bin 2>/dev/null
 cp scripts/* ~/bin 
-echo screenrc
+echo Enable mouse scrolling in screenrc...
 echo 'termcapinfo xterm* ti@:te@'|tee -a ~/.screenrc
 echo Can\'t do permissions and groups without root
 fi
 
-scripts/defaults
+./defaults
 
 echo Done\!
