b2f2c282ca6a9dfed410ffc2ffe8002718906d35
Coenraad <coenraad@wish.org.za>
2024-04-21 21:47:19 +0200

    ...

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cd "$(dirname -- "$0")" 2>/dev/null # allows script to be called from another di
 S=false
 echo -n "Install as root? (Y/n) "; read a; [ "$a" != "n" ] && [ "$a" != "N" ] && S=`which sudo`
 
-if whoami|grep root&&S="" >/dev/null || ($S whoami) >/dev/null; then # don't need sudo if we are root, else check if sudo exists
+if whoami|grep root&&S="" >/dev/null || ($S whoami) >/dev/null; then # don't need sudo if we are root, else check if sudo exists ------------
 echo Installing globally...
 which wget >/dev/null || {
   echo Installing wget...
@@ -20,11 +20,13 @@ which earlyoom >/dev/null || {
  }
 # todo copy from defaults or make more modular
 grep -q ^kernel.sysrq=1 /etc/sysctl.conf /etc/sysctl.d/*|| echo "kernel.sysrq=1" | $S tee -a /etc/sysctl.conf
+
 echo Trimming systemd journal...
 $S journalctl --vacuum-size=50M
-$S cp scripts/* /usr/local/bin # fixme move more to aliases, also clean up old utils still scattered on various machines?
+
 f=/etc/screenrc; echo $f # enable mouse wheel scroll in screen
 grep -q '^termcapinfo xterm* ti@:te@' $f || echo 'termcapinfo xterm* ti@:te@'|$S tee -a $f
+
 f=/etc/ssh/ssh_config; echo $f # enable socket reuse
 if ! grep -q ControlPersist $f; then 
 cat <<EOF | $S tee -a $f
@@ -33,18 +35,22 @@ cat <<EOF | $S tee -a $f
 	ControlPersist 600
 EOF
 fi 
+
+$S cp scripts/* /usr/local/bin # fixme move more to aliases, also clean up old utils still scattered on various machines?
 $S chgrp src /usr/src # create a src group that has access to /usr/src
 $S chmod g+s /usr/src
 $S usermod -a -G src `whoami` # add current user to src group
 $S mkdir -p /usr/src/github # todo - make git pulls from github pull here under username
 
-else # end global/root install 
-	echo Installing for `whoami` only... you\'re missing out on oom-killer, src group, ssh and screen configs, alt+sysrq+f\!
-	mkdir ~/bin 2>/dev/null
-	cp scripts/* ~/bin # this is in the path on most distributions
-	echo Enable mouse scrolling in screenrc...
-	echo 'termcapinfo xterm* ti@:te@'|tee -a ~/.screenrc
-q	echo Can\'t do permissions and groups without root
+else # do a user install instead  -----------------------------------------------------------------------------------------------------------
+
+echo Installing for `whoami` only... you\'re missing out on oom-killer, src group, ssh and screen configs, alt+sysrq+f\!
+mkdir ~/bin 2>/dev/null
+cp scripts/* ~/bin # this is in the path on most distributions
+echo Enable mouse scrolling in screenrc...
+echo 'termcapinfo xterm* ti@:te@'|tee -a ~/.screenrc
+echo Can\'t do permissions and groups without root
+
 fi 
 
 ./defaults
